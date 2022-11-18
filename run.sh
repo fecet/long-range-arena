c82fe13bb574b7d9f9aeee99e0fa1953cfa9c7f8
fecet <xiezej@gmail.com>
2022-11-18 22:37:30 +0800

    init

---
+++ run.sh
@@ -0,0 +1,7 @@
+xformer="local"
+
+PYTHONPATH="$(pwd)":"$PYTHON_PATH" python lra_benchmarks/listops/train.py \
+      --config="lra_benchmarks/listops/configs/${xformer}_base.py" \
+      --model_dir="/data/lra_results/${xformer}" \
+      --task_name=basic \
+      --data_dir=/data/lra_data/listops-1000/
